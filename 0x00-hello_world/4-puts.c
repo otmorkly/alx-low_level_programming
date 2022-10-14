b62a5b8c5566e2a05e9dd8c5df66ebc36ac4f5fe
otmorkly <morklyothniel@gmail.com>
2022-10-13 17:30:43 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing
+ *
+ * Return zero always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
